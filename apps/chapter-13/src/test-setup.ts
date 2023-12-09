4e221783130e446896102033378b7c2c9a47bf52
Dale Nguyen <dungnq@itbox4vn.com>
2023-12-09 11:15:57 -0500

    fix css parse in test

--- apps/chapter-13/src/test-setup.ts
+++ apps/chapter-13/src/test-setup.ts
@@ -6,3 +6,14 @@ globalThis.ngJest = {
   },
 }
 import 'jest-preset-angular/setup-jest'
+
+// Existing issue with jsdom
+// https://github.com/primefaces/primeng/issues/14085
+
+const originalConsoleError = console.error
+const jsDomCssError = 'Error: Could not parse CSS stylesheet'
+console.error = (...params) => {
+  if (!params.find((p) => p.toString().includes(jsDomCssError))) {
+    originalConsoleError(...params)
+  }
+}
